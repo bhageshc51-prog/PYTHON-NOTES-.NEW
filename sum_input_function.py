a845202136dfce84dafe367660ea90e771016eca
Bhagesh.c <bhageshc51@gmail.com>
2026-05-11 10:12:54 +0530

    Add files via upload

---
+++ sum_input_function.py
@@ -0,0 +1,15 @@
+# File Name: sum_input_function.py
+
+# Function Definition
+def sum(a, b):
+    return a + b
+
+# User Input
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+# Function Call
+x = sum(a, b)
+
+# Display Output
+print("Sum =", x)
